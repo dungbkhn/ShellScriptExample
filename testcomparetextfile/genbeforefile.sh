7465c023b510bb9db248b1bf9c1e2865dc69c71c
dungbkhn <nguyentuandung@gmail.com>
2022-09-24 10:34:43 +0700

    continue commit

--- testcomparetextfile/genbeforefile.sh
+++ testcomparetextfile/genbeforefile.sh
@@ -2,14 +2,28 @@
 shopt -s dotglob
 shopt -s nullglob
 
-f(){
-	if [ "$1" -lt 10 ] ; then
-		return 1
-	else
-		return 2
-	fi
+glb_mainmem_local="/home/dungnt/Backup/Store/MySyncDir"
+hashlogfile="hashlog.txt"
+glb_DirHash=""
+
+get_dir_hash(){
+	local dir_ori="$1"
+	local relative_path="$2"
+	local pathname
+	local dname
+			
+	for pathname in "$dir_ori"/* ; do
+		if [ -d "$pathname" ] ; then 
+			dname=$(basename "$pathname")
+			echo "$relative_path""/""$dname" >> "$hashlogfile"
+			glb_DirHash=$(stat "$pathname" -c '%Y')
+			echo "$glb_DirHash" >> "$hashlogfile"			
+			get_dir_hash "$pathname" "$relative_path""/""$dname"
+		fi
+	done
 }
 
-f 30
-
-echo "$?"
+glb_DirHash=$(stat "$glb_mainmem_local" -c '%Y')
+echo "/" >> "$hashlogfile"
+echo "$glb_DirHash" >> "$hashlogfile"
+get_dir_hash "$glb_mainmem_local" ""
